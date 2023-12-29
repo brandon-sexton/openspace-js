09b9f7902f491203ea51bec94ab0611999a31fec
sextonb <brandon.sexton@trueanomaly.space>
2023-12-29 09:36:45 -0700

    added scc pattern filter

--- src/data-handlers/SatCatHandler.js
+++ src/data-handlers/SatCatHandler.js
@@ -21,7 +21,7 @@ export class SatCat extends Array {
   /**
    * Loads the SatCat data from a local file.
    * @param {string} pathToSatCatFile - The path to the SatCat file.
-   * @return {SatCatHandler} The SatCat data as a handler object.
+   * @return {SatCat} The SatCat data as a handler object.
    */
   static fromJSON(pathToSatCatFile) {
     const data = require(pathToSatCatFile);
@@ -31,7 +31,7 @@ export class SatCat extends Array {
   /**
    * Filters the SatCat data by object types.
    * @param {Array<string>} objectTypes - The object types to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns satellites with an object type of 'PAYLOAD' or 'ROCKET BODY'.
    * const plAndRocketBody = handler.filterByTypes(['PAYLOAD', 'ROCKET BODY']);
@@ -49,7 +49,7 @@ export class SatCat extends Array {
   /**
    * Filters the SatCat by name patterns
    * @param {string} pattern - The pattern to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites with a name that starts with 'STARLINK'.
    * const starlinkSats = handler.filterByNamePattern('^STARLINK');
@@ -68,7 +68,7 @@ export class SatCat extends Array {
   /**
    * Filters the SatCat by country codes.
    * @param {Array<string>} countryCodes - The country codes to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites with a country code of 'US' or 'JP'.
    * const usAndJpSats = handler.filterByCountryCodes(['US', 'JP']);
@@ -76,9 +76,10 @@ export class SatCat extends Array {
   filterByCountryCodes(countryCodes) {
     return this.filter((sat) => countryCodes.includes(sat.COUNTRY));
   }
+
   /**
    * Filters the SatCat for objects that are still on orbit.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites that are still on orbit.
    * const stillOnOrbit = handler.filterByStillOnOrbit();
@@ -90,7 +91,7 @@ export class SatCat extends Array {
   /**
    * Filters the SatCat for objects that launched in a given year.
    * @param {Array<number>} years - The years to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites that launched in 2019 or 2020.
    * const launchedIn2019or2020 = handler.filterByLaunchYears([2019, 2020]);
@@ -103,7 +104,7 @@ export class SatCat extends Array {
    * Filters the SatCat for objects that launched in a given year range.
    * @param {number} startYear - The start year to filter by.
    * @param {number} endYear - The end year to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites that launched between 2018 and 2020.
    * const launchedIn2018To2020 = handler.filterByLaunchYearRange(2018, 2020);
@@ -119,7 +120,7 @@ export class SatCat extends Array {
    * Filters the SatCat for objects that have a perigee less than or equal to
    * a given value.
    * @param {number} maxPerigee - The maximum perigee to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites with a perigee less than or equal to 200 km.
    * const perigeeLessThan200 = handler.filterByMaxPerigee(200);
@@ -135,7 +136,7 @@ export class SatCat extends Array {
    * Filters the SatCat for objects that have an apogee greater than or equal to
    * a given value.
    * @param {number} minApogee - The minimum apogee to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    * @example
    * // Returns all satellites with an apogee greater than or equal to 200 km.
    * const apogeeGreaterThan200 = handler.filterByMinApogee(200);
@@ -150,9 +151,25 @@ export class SatCat extends Array {
   /**
    * Filters the SatCat for objects with a given NORAD ID.
    * @param {Array<number>} noradIds - The NORAD IDs to filter by.
-   * @return {Array<Object>} The filtered SatCat data.
+   * @return {SatCat} The filtered SatCat data.
    */
   filterByNoradIds(noradIds) {
     return this.filter((sat) => noradIds.includes(sat.NORAD_CAT_ID));
+  }
+
+  /**
+   * Filters the SatCat for objects with a NORAD ID that matches a pattern.
+   * @param {string} pattern - The pattern to filter by.
+   * @return {SatCat} The filtered SatCat data.
+   * @example
+   * // Returns all satellites with a NORAD ID that starts with '255'.
+   * const sats = handler.filterByNoradIdPattern('^25544');
+   * @example
+   * // Returns all satellites with a NORAD ID that contains '255'.
+   * const sats = handler.filterByNoradIdPattern('255');
+   */
+  filterByNoradIdPattern(pattern) {
+    const regex = new RegExp(pattern, 'i');
+    return this.filter((sat) => regex.test(sat.NORAD_CAT_ID));
   }
 };
